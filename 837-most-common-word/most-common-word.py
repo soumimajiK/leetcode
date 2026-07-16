bab6ada7ebd882941ce2700b61462862b3dd32d5
Soumi Maji <iamsoumimajiii@gmail.com>
2026-07-16 15:02:34 +0530

    Time: 4 ms (28.16%) | Memory: 19.4 MB (51.68%) - LeetSync

--- 837-most-common-word/most-common-word.py
+++ 837-most-common-word/most-common-word.py
@@ -1,17 +1,19 @@
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        import string
+        banned = set(banned)
+        paragraph = "".join([c.lower() if c.isalnum() else " " for c in paragraph])
+        words = paragraph.split()
+        print(words)
 
-        count={}
-        mV=0
-        mI=""
-        translator = str.maketrans(string.punctuation, " " * len(string.punctuation))
-        arrA = paragraph.lower().translate(translator).split()
-        for i in arrA:
-            count[i]=count.get(i, 0) + 1
-        for idx, val in count.items():
-            if idx not in banned:
-                if val>mV:
-                    mV=val
-                    mI=idx
-        return mI
+        hs = {}
+        mxf, mxw = 0, None
+
+        for w in words:
+            if w in banned:
+                continue
+            hs[w] = hs.get(w, 0) + 1
+            if hs[w] > mxf:
+                mxf = hs[w]
+                mxw = w
+
+        return mxw
